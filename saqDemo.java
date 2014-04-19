e5947b0e55c7d767b9981afb9170cf04283862e2
kawikadkekahuna <kawikakekahuna@gmail.com>
2014-04-19 00:40:39 -1000

    Create saqDemo.java

---
+++ saqDemo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class saqDemo {
+
+	private static intQueue queue = new intQueue();
+	private static intStack stack = new intStack();
+
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		
+			//stack.prompt();
+			queue.prompt();
+			
+	
+
+	}
+
+}
